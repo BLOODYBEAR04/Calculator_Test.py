ff04ef04c97a6c5e388ef156a6f7dee275a676a6
*NEKO* <102714031+BLOODYBEAR04@users.noreply.github.com>
2022-11-05 19:42:26 +0800

    Add files via upload

---
+++ Calculator_Test.py
@@ -0,0 +1,20 @@
+import math
+
+num1 = input("Enter 1st number: ")
+oper = input("Enter desired operation(+,-,&,/): ")
+num2 = input("Enter 2nd number: ")
+
+if oper == "+":
+    result = float(num1) + float(num2)
+    print(num1, "+", num2, "=", result)
+elif oper == "-":
+    result = float(num1) - float(num2)
+    print(num1, "-", num2, "=", result)
+elif oper == "*":
+    result = float(num1) * float(num2)
+    print(num1, "*", num2, "=", result)
+elif oper == "/":
+    result = float(num1) / float(num2)
+    print(num1, "/", num2, "=", result)
+else:
+    print("WRONG SYNTAX")
